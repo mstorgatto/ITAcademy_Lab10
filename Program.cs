4e7465052e442f6b54f3e0916d6b0e30d7d3cb13
JornadaStorgattoVescovini <Marinai_Storgatto@Dellteam.com>
2021-08-13 14:18:10 -0300

    Programa com GroupBy

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Pessoa> pessoas = new List<Pessoa>
+           {
+               new Pessoa{Nome = "Ana", DataNascimento = new DateTime(1980, 3, 14), Casada = true},
+               new Pessoa{Nome = "Paulo", DataNascimento = new DateTime(1978, 10, 23), Casada = true},
+               new Pessoa{Nome = "Maria", DataNascimento = new DateTime(2000, 1, 10), Casada = false},
+               new Pessoa{Nome = "Carlos", DataNascimento = new DateTime(1999, 12, 12), Casada = true}
+           };
+           
+           //3.1
+
+           var linq5 = pessoas.GroupBy(
+               Pessoa => Pessoa.Casada,
+               (pessoaCasadas, pessoas) => new
+               {
+                   Key = pessoaCasadas,
+                   Count = pessoas.Count(),
+                   ListaCasados = pessoas,
+               });
+
+               foreach (var result in linq5)
+               {
+                   Console.WriteLine("\nCasados: " + result.Key);
+                   Console.WriteLine("Numero de pessoas no grupo: " + result.Count);
+                   foreach (var i in result.ListaCasados)
+                   {
+                       Console.WriteLine(i.Nome);
+                   }
+                   
+               }
+           
+                       
+           
+           
+        //    //tipo1 de linq           
+        //     var linq1 = from p in pessoas 
+        //                where p.Casada
+        //                select p;
+
+        //     foreach (var p in linq1)
+        //     {
+        //         Console.WriteLine(p);
+        //     }
+
+        //     //tipo2 de linq
+        //     var linq2 = pessoas.Where(p => p.Casada);
+        //     foreach (var p in linq2)
+        //     {
+        //         Console.WriteLine(p);
+        //     }
+
+        //     var linq3 = from p in pessoas
+        //                 where p.Casada && p.DataNascimento > new DateTime(1980, 1, 1)
+        //                 select p.Nome;
+
+        //     //Console.WriteLine(linq3.ToList());
+        //     linq3.ToList().ForEach(Console.WriteLine);
+
+        //     var linq4 = pessoas.Where(p => p.Casada).Count();
+        //     Console.WriteLine(linq4);
+        }
+    }
+}
